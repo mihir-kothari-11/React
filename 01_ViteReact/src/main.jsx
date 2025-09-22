2c4fa5d439150c34b4c7aabf3be52803a175cd38
Mihir Kothari <kotharimihir07@gmail.com>
2025-09-22 09:51:42 +0530

    Create a custome React  wih using a react.

--- 01_ViteReact/src/main.jsx
+++ 01_ViteReact/src/main.jsx
@@ -1,9 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from "react"; 
 import App from './App.jsx'
+
+
+const reactElement = React.createElement(
+    'a',
+    {href : 'https://flipkart.com', targer:'_blank'},
+    'visit flipkart'
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   { reactElement}
+   <App/>
   </StrictMode>,
 )
