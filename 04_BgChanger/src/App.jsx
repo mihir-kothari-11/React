74bbe890bb2f650f1e6668a52f869e3d5420a210
Mihir Kothari <kotharimihir07@gmail.com>
2025-09-24 10:43:08 +0530

    Black color added in bgChanger

--- 04_BgChanger/src/App.jsx
+++ 04_BgChanger/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
           <button onClick={() => setColor("green")} className="bg-green-500 w-16 h-16 rounded" style={{backgroundColor:"green"}}></button>
           <button onClick={() => setColor("orange")} className="bg-yellow-500 w-16 h-16 rounded" style={{backgroundColor:"orange"}}></button>
           <button onClick={() => setColor("purple")} className="bg-purple-500 w-16 h-16 rounded" style={{backgroundColor:"purple"}}></button>
+          <button onClick={() => setColor("black")} className="bg-black-500 w-16 h-16 rounded" style={{backgroundColor:"black"}}></button>
         </div>
       </div>
       </div>
